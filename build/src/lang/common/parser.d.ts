c66d3c7ee4e5090ef0d424eb40f0e7a79c61b3ef
Takeshi <iwatakeshi@gmail.com>
2018-08-27 12:33:07 -0600

    Chore: Build files

--- build/src/lang/common/parser.d.ts
+++ build/src/lang/common/parser.d.ts
@@ -6,12 +6,14 @@ import { XDocParserOptions } from "xdoc-parser/src/XDocParser";
 export interface ParserOptions {
     log: LogOptions;
     documentation: XDocParserOptions;
+    language?: string;
 }
 export default abstract class Parser {
     private source_;
     protected options: ParserOptions;
     constructor(source: Source, options: Partial<ParserOptions>);
     readonly source: Source;
+    readonly language: string;
     abstract parse(): ASTNode[];
     abstract readonly tree: Tree;
 }
